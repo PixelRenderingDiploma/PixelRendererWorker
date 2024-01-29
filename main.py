97152ae4af2abbc6f222114a7bd745bd1100292a
Hlebushkek <volsoor@gmail.com>
2024-01-29 04:33:21 -0800

    add upscaling + video creating

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import subprocess
 import time
 import json
 import os
+import shutil
 
 from azure_manager import AzureManager
 
@@ -14,13 +15,6 @@ def create_processing_folders(folder_path):
         os.makedirs(folder_path)
         print(f"Created folder: {folder_path}")
 
-    subfolders = ['configs', 'models', 'processing']
-    for subfolder in subfolders:
-        subfolder_path = os.path.join(folder_path, subfolder)
-        if not os.path.exists(subfolder_path):
-            os.makedirs(subfolder_path)
-            print(f"Created subfolder: {subfolder_path}")
-
 def update_config_status(config_path, status):
     with open(config_path, 'r') as file:
         config = json.load(file)
@@ -29,12 +23,12 @@ def update_config_status(config_path, status):
     with open(config_path, 'w') as file:
         json.dump(config, file)
 
-def run_blender_rendering(file_path):
+def run_blender_rendering(file_path, output_folder):
     print('Starting rendering process: ', os.path.splitext(os.path.basename(file_path))[0])
 
     dir = os.path.dirname(os.path.abspath(__file__))
     input_path = os.path.join(dir, file_path)
-    output_path = os.path.join(dir, f"results/processing/" + os.path.splitext(os.path.basename(file_path))[0] + ".png")
+    output_path = os.path.join(dir, output_folder)
     
     command = "python ../PixelRendererBlender/blender_rendering.py --input=\"{}\" --output=\"{}\"".format(input_path, output_path)
 
@@ -48,18 +42,6 @@ def run_blender_rendering(file_path):
             break
         if output:
             print(output.strip())
-    # try:
-    #     while process.poll() is None:  # Continue monitoring until the subprocess completes
-    #         pass
-    #         try:
-    #             cpu_usage, ram_usage = get_system_metrics(subprocess_pid)
-    #             time.sleep(1)  # Add a brief sleep to avoid excessive polling
-    #         except (psutil.NoSuchProcess, psutil.ZombieProcess):
-    #             # Zombie process detected, ignore the exception and continue
-    #             pass
-    # except Exception as e:
-    #     # Handle other exceptions that may occur during monitoring
-    #     print(f"An error occurred during monitoring: {e}")
     
     process.wait()
 
@@ -67,31 +49,66 @@ def run_blender_rendering(file_path):
     
     elapsed_time_minutes = (end_time - start_time) / 60
 
-    if not os.path.exists(os.path.join(output_path)):
-        raise FileNotFoundError(f"The file 'renderer.png' does not exist at path '{output_path}'.")
+    if not os.listdir(output_path):
+        raise FileNotFoundError(f"The rendering folder is empty at path '{output_path}'")
     else:
-        print(f"Rendered png file at path: '{output_path}'.")
-    
-def process_file(file_path, config):
-    print("Processing file: ", file_path)
-    print("Processing config: ", config)
+        print(f"Rendered images at path: '{output_path}'")
 
-    config_path = 'results/configs/' + config["id"] + '.json'
-    update_config_status(config_path, status='processing')
+def run_video_generation(input_folder, video_path):
+    print('Starting composing process: ', os.path.splitext(os.path.basename(input_folder))[0])
+
+    dir = os.path.dirname(os.path.abspath(__file__))
+    input_path = os.path.join(dir, input_folder)
+    output_path = os.path.join(dir, video_path)
+    
+    command = "python ../PixelRendererBlender/resize.py --input=\"{}\" --output=\"{}\"".format(input_path, output_path)
+
+    start_time = time.time()
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    subprocess_pid = process.pid
+
+    while True:
+        output = process.stdout.readline()
+        if process.poll() is not None:
+            break
+        if output:
+            print(output.strip())
+    
+    process.wait()
+
+    end_time = time.time()
+    
+    elapsed_time_minutes = (end_time - start_time) / 60
+
+    if not os.path.exists(output_path):
+        raise FileNotFoundError(f"The video does not exists at path '{output_path}'")
+    else:
+        print(f"Rendered video at path: '{output_path}'")
+    
+def process_directory(working_directory, config):
+    print("Processing directory: ", working_directory)
+
+    model_path = working_directory + config["id"] + '.glb'
+    config_path = working_directory + config["id"] + '.json'
+
+    rendered_directory = working_directory + "rendered_images/"
+
+    video_folder = working_directory + "video/"
+    video_path = video_folder + config["id"] + ".mp4"
+    if not os.path.exists(video_folder):
+        os.makedirs(video_folder)
+
+    update_config_status(config_path, status='rendering')
     azure_manager.put_media_path(config_path, "configs/models/" + config["id"] + '.json', config["id_token"])
 
-    run_blender_rendering(file_path)
+    run_blender_rendering(model_path, rendered_directory)
+    update_config_status(config_path, status='composing')
+    azure_manager.put_media_path(config_path, "configs/models/" + config["id"] + '.json', config["id_token"])
 
+    run_video_generation(rendered_directory, video_path)
     update_config_status(config_path, status='done')
     azure_manager.put_media_path(config_path, "configs/models/" + config["id"] + '.json', config["id_token"])
-
-    output_path = 'results/processing/' + config["id"] + '.png'
-    azure_manager.put_media_path(output_path, "renders/image/" + config["id"] + '.png', config["id_token"])
-
-    os.remove(config_path)
-    os.remove(file_path)
-    os.remove(output_path)
-
+    azure_manager.put_media_path(video_path, "renders/videos/" + config["id"] + '.mp4', config["id_token"])
 
 def process_queue_element():
     config = azure_manager.get_next_config()
@@ -101,14 +118,19 @@ def process_queue_element():
 
     config_name = config['id']
 
-    with open('results/configs/' + config_name + '.json', 'w') as file:
+    working_folder = "results/" + config_name + "/"
+    if not os.path.exists(working_folder):
+        os.makedirs(working_folder)
+
+    with open(working_folder + config_name + '.json', 'w') as file:
         json.dump(config, file)
 
     if config:
-        local_path = 'results/' + config["model"]
-        resp = azure_manager.get_media(local_path, config["model"], config["id_token"])
-
-        process_file(local_path, config)
+        model_path = working_folder + config_name + ".glb"
+        resp = azure_manager.get_media(model_path, config["model"], config["id_token"])
+        
+        process_directory(working_folder, config)
+        shutil.rmtree(working_folder)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Rendering Queue App")
